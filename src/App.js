021172c37451e2be5965078adc75fe45b103d00b
Homma <ab13891012.gmail.com>
2019-03-09 17:27:41 +0900

    delete Unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Welcome to react
-      </div>
-    );
+    return ( <h1>あああ</h1> );
   }
 }
 
